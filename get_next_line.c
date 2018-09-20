b647e544162a86426fa411785f590279a4453b65
thk2b <theokobb@gmail.com>
2018-09-20 11:31:00 -0700

    work

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/19 10:07:28 by tkobb             #+#    #+#             */
-/*   Updated: 2018/09/20 09:21:51 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/09/20 11:28:49 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,45 @@ static char	*alloc_line(const char *start, const char *end)
 	return (s);
 }
 
+static int	fill_buff(int fd, t_buff *buff, char **line)
+{
+	char	*tmp;
+	char	*tmp1;
+	char	*nl;
+	ssize_t	nread;
+
+	tmp = NULL;
+	while ((nread = read(fd, buff->data, BUFF_SIZE)) > 0)
+	{
+		if ((nl = ft_strchr(buff->data, '\n')))
+		{
+			*nl = '\0';
+			if (tmp != NULL)
+			{
+				*line = ft_strjoin(tmp, buff->data);
+				free(tmp);
+			}
+			else
+				*line = ft_strdup(buff->data);
+			buff->cur = nl + 1;
+			return (1);
+		}// buff is too small
+		if (tmp == NULL)
+			tmp = ft_strdup(buff->data);
+		else
+		{
+			tmp1 = tmp;
+			tmp = ft_strjoin(tmp, buff->data);
+			free(tmp1);
+		}
+	}
+	return (nread);
+}
+
 int		get_next_line(int fd, char **line)
 {
-	static t_buff	c = {
-		{0},
-		c.buf + BUFF_SIZE,
-		c.buf
-	};
+	static t_buff	c = {{0}, c.data};
 	char			*nl;
-	ssize_t			nread;
 
 	if ((nl = ft_strchr(c.cur, '\n')))
 	{
@@ -43,23 +73,11 @@ int		get_next_line(int fd, char **line)
 		c.cur = nl + 1;
 		return (1);
 	}
-	if (c.cur != c.buf) // buf is not empty
+	if (c.cur != c.data) // buf is not empty
 	{
-		*line = alloc_line(c.cur, c.buf + BUFF_SIZE);
-		c.cur = c.buf;
+		*line = alloc_line(c.cur, c.data + BUFF_SIZE);
+		c.cur = c.data;
 	}
-	ft_bzero(c.buf, BUFF_SIZE);
-	while ((nread = read(fd, c.buf, BUFF_SIZE)) > 0)
-	{
-		if ((nl = ft_strchr(c.buf, '\n')))
-		{
-			*line = alloc_line(c.cur, nl);
-			c.cur = nl + 1;
-			return (1);
-		}
-		return (-1); // buff too small
-	}
-	if (nread == -1)
-		return (-1);
-	return (0);
+	ft_bzero(c.data, BUFF_SIZE);
+	return (fill_buff(fd, &c, line));
 }
