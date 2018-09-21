c2dd0bd2f97bc33c573b991abe0a01e72cf1b703
thk2b <theokobb@gmail.com>
2018-09-20 20:20:58 -0700

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,16 +6,18 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/19 10:07:28 by tkobb             #+#    #+#             */
-/*   Updated: 2018/09/20 17:57:04 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/09/20 20:20:26 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "get_next_line.h"
+#include <printf.h>
 
 static int	copy_line(t_buff *buff, char *nl, char *tmp, char **line)
 {
-	*nl = '\0';
+	if (nl)
+		*nl = '\0';
 	if (tmp != NULL)
 	{
 		*line = ft_strjoin(tmp, buff->data);
@@ -23,6 +25,8 @@ static int	copy_line(t_buff *buff, char *nl, char *tmp, char **line)
 	}
 	else
 		*line = ft_strdup(buff->cur);
+	if (nl == NULL)
+		return (0);
 	buff->cur = nl + 1;
 	return (1);
 }
@@ -33,10 +37,13 @@ static int	fill_buff(int fd, t_buff *buff, char **line)
 	char	*tmp1;
 	char	*nl;
 	ssize_t	nread;
+	ssize_t	tread;
 
 	tmp = *line;
+	tread = 0;
 	while ((nread = read(fd, buff->data, BUFF_SIZE)) > 0)
 	{
+		tread += nread;
 		if ((nl = ft_strchr(buff->data, '\n')))
 			return (copy_line(buff, nl, tmp, line));
 		if (tmp == NULL)
@@ -48,7 +55,9 @@ static int	fill_buff(int fd, t_buff *buff, char **line)
 			free(tmp1);
 		}
 	}
-	return (nread);
+	if (tread && nread == 0)
+		*line = tmp;
+	return (tread >= 1 ? 1 : nread);
 }
 
 int			get_next_line(int fd, char **line)
@@ -56,6 +65,8 @@ int			get_next_line(int fd, char **line)
 	static t_buff	c = {{0}, c.data};
 	char			*nl;
 
+	if (fd < 0 || line == NULL || read(fd, c.data, 0) < 0)
+		return (-1);
 	if ((nl = ft_strchr(c.cur, '\n')))
 		return (copy_line(&c, nl, NULL, line));
 	if (c.cur != c.data)
