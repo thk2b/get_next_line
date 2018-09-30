2c59b3fcdc850b2ab84cb2f7bdad8a5e3d7fee67
thk2b <theokobb@gmail.com>
2018-09-30 00:10:00 -0700

    norm

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,47 @@
 /*   By: tkobb <tkobb@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/20 22:41:11 by tkobb             #+#    #+#             */
-/*   Updated: 2018/09/29 23:08:06 by tkobb            ###   ########.fr       */
+/*   Updated: 2018/09/30 00:07:16 by tkobb            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft.h"
-#include <unistd.h>
-#include <printf.h>
+#include "libft/includes/libft.h"
+
+int		next_line(int fd, char **line, char **nl, char **tmp)
+{
+	char		*tmp1;
+	ssize_t		nread;
+	char		buf[BUFF_SIZE + 1];
+
+	while ((*nl = ft_strchr(*tmp, '\n')) == NULL)
+	{
+		ft_bzero(buf, BUFF_SIZE + 1);
+		if ((nread = read(fd, buf, BUFF_SIZE)) == -1)
+			return (-1);
+		if (nread == 0)
+		{
+			if (**tmp == '\0')
+				return (0);
+			*line = ft_strdup((*tmp));
+			**tmp = '\0';
+			return (1);
+		}
+		tmp1 = *tmp;
+		*tmp = ft_strjoin(*tmp, buf);
+		free(tmp1);
+	}
+	return (2);
+}
 
 int		get_next_line(int fd, char **line)
 {
-	static char	*data[MAX_FD] = {NULL};
-	char		buf[BUFF_SIZE + 1];
+	static char	*data[MAX_FD] = {0};
 	char		*nl;
 	char		*tmp;
-	char		*tmp1;
-	ssize_t		nread;
+	int			ret;
 
-	if (line == NULL || fd < 0 || read(fd, buf, 0) < 0)
+	if (line == NULL || fd < 0 || read(fd, NULL, 0) < 0)
 		return (-1);
 	if (data[fd] && (nl = ft_strchr(data[fd], '\n')))
 	{
@@ -36,23 +58,8 @@ int		get_next_line(int fd, char **line)
 	tmp = data[fd];
 	if (tmp == NULL)
 		ALLOC_CHECK(tmp = ft_strdup(""));
-	while ((nl = ft_strchr(tmp, '\n')) == NULL)
-	{
-		ft_bzero(buf, BUFF_SIZE + 1);
-		if ((nread = read(fd, buf, BUFF_SIZE)) == -1)
-			return (-1);
-		if (nread == 0)
-		{
-			if (*tmp == '\0')
-				return (0);
-			*line = ft_strdup(tmp);
-			*tmp = '\0';
-			return (1);
-		}
-		tmp1 = tmp;
-		tmp = ft_strjoin(tmp, buf);
-		free(tmp1);
-	}
+	if ((ret = next_line(fd, line, &nl, &tmp)) != 2)
+		return (ret);
 	*nl = '\0';
 	*line = ft_strdup(tmp);
 	data[fd] = ft_strdup(nl + 1);
